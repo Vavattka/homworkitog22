182a530c6cdb5b7f74d4f62b5970f87c9b195030
Vavattka <106929881+Vavattka@users.noreply.github.com>
2022-09-21 22:11:58 +0300

    Add files via upload

---
+++ project.js
@@ -0,0 +1,50 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    const slides = document.getElementsByClassName("mySlides");
+    const dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
+    ymaps.ready(init);
+    var myMap;
+
+    function init() {
+
+    myMap = new ymaps.Map("map", {
+        center: [53.926469327912606, 27.517519711109795],
+        zoom: 13
+    });
+
+    myMap.controls.add(
+    new ymaps.control.ZoomControl()
+    );
+
+    myPlacemark = new ymaps.Placemark([53.926469327912606, 27.517519711109795], {
+    balloonContent: "<div class='ya_map'>Заезжайте в гости!</div>"
+}, {
+    preset: "twirl#redDotIcon"
+});
+
+    myMap.geoObjects.add(myPlacemark);
+    myPlacemark.balloon.open();
+
+}
